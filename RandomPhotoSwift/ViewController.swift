ebf82e07b945650781e6cb47db6f2ebd2bf01838
Chase Kessler <chasekessler30@yahoo.com>
2021-12-10 14:26:47 -0500

    Initial Commit

---
+++ RandomPhotoSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhotoSwift
+//
+//  Created by Chase Kessler on 12/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
